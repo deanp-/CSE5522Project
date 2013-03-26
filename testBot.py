60e3aa4729db385e4b9bfb0b13de399f210d219e
Andrew McKain <mckain.7@osu.edu>
2013-03-26 17:57:26 -0400

    Added Feature Vectors for individual bots
    Added a function which returns a dictionary containing the (name,
    feature Vector) pair for each bot on the team.
    
    As a note, total game time is constant (300 seconds)
    All distances in the feature vector are normalized by the diagonal
    distance of the entire map (the farthest conceivable distance possible)
    but if this is incorrect feel free to change it.

--- testBot.py
+++ testBot.py
@@ -25,6 +25,8 @@ class TestCommander(Commander):
 
 	teamName = ""
 	goalLoc = Vector2(0,0)
+	respawnTime = 0
+	maxRespawnTime = 45
 
 	def initialize(self):
 		#grabs initial game state for the game
@@ -54,7 +56,8 @@ class TestCommander(Commander):
 		holdingBots = gameState.bots_holding
 		enemyFlagInfo = gameState.enemyFlags
 		teamName = myInfo.name
-
+                self.respawnTime = matchInfo.timeToNextRespawn
+    
 		"""Initialize reinforcement learning"""
 		goalPoint = teamInfo[teamName].flagSpawnLocation
 		environment = Maze(structure, (goalPoint.x,goalPoint.y))
@@ -70,23 +73,34 @@ class TestCommander(Commander):
 		#agent.reset()
 
 	def tick(self):
+
+                #global feature vector
 		fv = self.getFeatureVector()
-		print fv
+		#feature vectors for each bot
+		bfvs = self.getBotFeatureVectors()
 		
-        	for bot in self.game.bots_available:
-            		if bot.flag:
-                	# if a bot has the flag run to the scoring location
-                		flagScoreLocation = self.game.team.flagScoreLocation
-                		self.issue(commands.Charge, bot, flagScoreLocation, description = 'Run to my flag')
-            		else:
-                	# otherwise run to where the flag is
-                		enemyFlag = self.game.enemyTeam.flag.position
-                		self.issue(commands.Charge, bot, enemyFlag, description = 'Run to enemy flag')
+		#print bfvs
+		#print fv
+		
+                for bot in self.game.bots_available:
+                        if bot.flag:
+                        # if a bot has the flag run to the scoring location
+                                flagScoreLocation = self.game.team.flagScoreLocation
+                                self.issue(commands.Charge, bot, flagScoreLocation, description = 'Run to my flag')
+                        else:
+                        # otherwise run to where the flag is
+                                enemyFlag = self.game.enemyTeam.flag.position
+                                self.issue(commands.Charge, bot, enemyFlag, description = 'Run to enemy flag')
 			
 	def shutdown(self):
 		pass
 	
 	def getFeatureVector(self):
+                """
+                        Gets the global feature vector for the game
+                        Returns a list of numeric values
+                """
+                
 		#grabs initial game state for the game
 		gameState = self.game
 		#grabs initial level state for the level
@@ -119,8 +133,6 @@ class TestCommander(Commander):
 		ourFlag = myInfo.flag.name
 		if(myInfo.flag.carrier != None):
 			ourFlagCarried = 1
-		#distance between the bots and enemy flag
-		#distance between the bots and the team's flag
 		#distance from their flag carrier from our score point
 		myFlagTotalDist = enemyInfo.flagSpawnLocation.distance(myInfo.flagScoreLocation)
 		myFlagDist = enemyInfo.flag.position.distance(myInfo.flagScoreLocation)
@@ -129,8 +141,10 @@ class TestCommander(Commander):
 		theirFlagTotalDist = myInfo.flagSpawnLocation.distance(enemyInfo.flagScoreLocation)
 		theirFlagDist = myInfo.flag.position.distance(enemyInfo.flagScoreLocation)
 		theirFlagDist = theirFlagDist/theirFlagTotalDist
+		#total respawn time (max respawn time is 45 seconds from observation)
+		respawnTime = self.respawnTime/self.maxRespawnTime
 		#time until respawn
-		respawnTime = matchInfo.timeToNextRespawn/lvlState.respawnTime
+		timeToRespawn = matchInfo.timeToNextRespawn/lvlState.respawnTime
 		#percentage of time remaining in the game
 		timeR = matchInfo.timeRemaining
 		timeP = matchInfo.timePassed
@@ -153,19 +167,122 @@ class TestCommander(Commander):
 		elif(enemyScore == myScore):
 			scoreDiff = .5
 
-		
 		featureVector = [
 				flagCarried, 
 				ourFlagCarried,
 				myFlagDist, 
 				theirFlagDist,
-				respawnTime, 
+                                respawnTime,
+				timeToRespawn, 
 				timeLeft, 
 				totalBots, 
 				enemyNumber, 
 				scoreDiff]
 		return featureVector
 	
-if __name__ == '__main__':
-	c = TestCommander("a")
-	c.initialize();
+	def getBotFeatureVectors(self):
+                """
+                        Gets the feature vector for individual bots on the team
+                        Returns a dictionary containing the bot names and their
+                        corresponding feature vectors
+                        i.e. {"red1": [1,0,1,....], "red2": [0,1,1,....], ......}
+                """
+                
+                fvDict = {}
+
+		#grabs initial game state for the game
+		gameState = self.game
+		#grabs initial level state for the level
+		lvlState = self.level
+
+		#normalizing distance (farthest distance possible, 1 corner to the other)
+		#refactor to more global scope later (value does not change)
+		d1 = lvlState.area[0]
+                d2 = lvlState.area[1]
+                normDist = d1.distance(d2)
+                
+		"""information from the game state"""
+		matchInfo = gameState.match
+		teamInfo = gameState.teams
+		myInfo = gameState.team
+		enemyInfo = gameState.enemyTeam
+		botInfo = gameState.bots
+		flagInfo = gameState.flags
+		
+                #generate feature vector for each bot
+                for bot in myInfo.members:
+
+                        #current position of the bot
+                        botPos = bot.position
+                        
+                        #HasFlag
+                        hasFlag = 0
+                        if bot.flag != None:
+                                hasFlag = 1
+                        #Health
+                        isAlive = 0
+                        if bot.health > 0:
+                                isAlive = 1
+                        #See enemy
+                        seesEnemy = 0
+                        if len(bot.visibleEnemies) > 0:
+                                seesEnemy = 1
+                        #Seen by enemy
+                        seenByEnemy = 0
+                        if len(bot.seenBy) > 0:
+                                seenByEnemy = 1
+                        #distance to enemy FC
+                        distToEnemyFC = 0
+                        if myInfo.flag.carrier != None:
+                                distToEnemyFC = botPos.distance(myInfo.flag.carrier.position)/normDist
+                        #distance to our flag/FC
+                        distToFriendlyFC = 0
+                        if enemyInfo.flag.carrier != None:
+                                distToFriendlyFC = botPos.distance(enemyInfo.flag.carrier.position)/normDist
+                        #distance to nearest seen enemy
+                        distToNearestEnemy = 1
+                        if len(bot.visibleEnemies) > 0:
+                                for enBot in bot.visibleEnemies:
+                                        tDist = botPos.distance(enBot.position)/normDist
+                                        if tDist < distToNearestEnemy:
+                                                distToNearestEnemy = tDist
+                        #distance to nearest ally
+                        distToNearestAlly = 1
+                        for frBot in myInfo.members:
+                               if frBot != bot:
+                                       tDist = botPos.distance(frBot.position)/normDist
+                                       if tDist < distToNearestAlly:
+                                               distToNearestAlly = tDist
+
+                        """NOT WORKING, NOT INCLUDED IN VECTOR"""
+                        #distance to combat/ally support
+                        distToNearestCombat = 1
+                        for btBot in gameState.bots_holding:
+                                tDist = botPos.distance(btBot.position)/normDist
+                                if tDist < distToNearestCombat:
+                                        distanceToNearestCombat = tDist
+
+                        #distance to friendly spawn zone
+                        mySpawn = myInfo.botSpawnArea
+                        distToFriendlySpawn = botPos.distance(mySpawn[0].midPoint(mySpawn[1]))/normDist
+                        #distance to enemy spawn zone
+                        enemySpawn = enemyInfo.botSpawnArea
+                        distToEnemySpawn = botPos.distance(enemySpawn[0].midPoint(enemySpawn[1]))/normDist
+
+                       
+                        #feature vector for individual bot
+                        botFV = [
+                                hasFlag,
+                                isAlive,
+                                seesEnemy,
+                                seenByEnemy,
+                                distToEnemyFC,
+                                distToFriendlyFC,
+                                distToNearestEnemy,
+                                distToNearestAlly,
+                                distToFriendlySpawn,
+                                distToEnemySpawn]
+
+                        fvDict[bot.name] = botFV
+                        
+                return fvDict
